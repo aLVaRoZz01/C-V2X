f48200d7a0b54fcddd999f60210cacc516c4953e
aLVaRoZz01 <alvaro.rosado.g@gmail.com>
2023-05-08 12:35:51 +0200

    sNavigator2 en proceso de cominicacion bidireccional

---
+++ src/apps/sNavigator2/SNavigatorModule2.h
@@ -0,0 +1,96 @@
+//
+//                  Simu5G
+//
+// Authors: Giovanni Nardini, Giovanni Stea, Antonio Virdis (University of Pisa)
+//
+// This file is part of a software released under the license included in file
+// "license.pdf". Please read LICENSE and README files before using it.
+// The above files and the present reference are part of the software itself,
+// and cannot be removed from it.
+//
+
+#ifndef _SNAVIGATORMODULE2_H_
+#define _SNAVIGATORMODULE2_H_
+
+#include <list>
+#include <string.h>
+
+#include <omnetpp.h>
+
+#include <inet/networklayer/common/L3AddressResolver.h>
+#include <inet/transportlayer/contract/udp/UdpSocket.h>
+#include "apps/sNavigator2/SNavigatorPacket2_m.h"
+
+#include "veins/modules/mobility/traci/TraCIMobility.h"
+#include "veins/modules/mobility/traci/TraCICommandInterface.h"
+#include "veins_inet/VeinsInetMobility.h"
+
+using veins::VeinsInetMobility;
+
+
+using namespace std;
+using namespace inet;
+using namespace veins;
+
+class SNavigatorModule2 : public omnetpp::cSimpleModule
+{
+    inet::UdpSocket socket;
+
+    //source
+    omnetpp::simtime_t durTalk_;
+    omnetpp::cMessage* selfSource_;
+    //sender
+    int iDtalk_;
+    int size_;
+    int type_;
+    std::string navMessage_;
+
+    unsigned int totalSentBytes_;
+
+    omnetpp::simsignal_t sNavigatorGeneratedThroughtput_;
+    // ----------------------------
+
+    omnetpp::cMessage *selfSender_;
+
+    omnetpp::cMessage *initTraffic_;
+
+    omnetpp::simtime_t timestamp_;
+    int localPort_;
+    int destPort_;
+    inet::L3Address destAddress_;
+
+    ~SNavigatorModule2();
+
+    typedef std::list<SNavigatorPacket2*> PacketsList;
+    PacketsList mPacketsList_;
+    PacketsList mPlayoutQueue_;
+    unsigned int mCurrentTalkspurt_;
+
+    bool mInit_;
+
+    unsigned int totalRcvdBytes_;
+    omnetpp::simtime_t warmUpPer_;
+
+    void initTraffic();
+    void selectPeriodTime();
+    void sendsNavigatorPacket();
+
+public:
+    SNavigatorModule2();
+
+protected:
+    /* pointers ill be set when used with TraCIMobility */
+    VeinsInetMobility* mobility;
+    TraCICommandInterface* traci;
+    TraCICommandInterface::Vehicle* traciVehicle;
+    std::string carId; // this vehicle
+
+  protected:
+
+    virtual int numInitStages() const override { return inet::NUM_INIT_STAGES; }
+    void initialize(int stage) override;
+    void handleMessage(omnetpp::cMessage *msg) override;
+};
+
+#endif
+
---
+++ src/apps/sNavigator2/SNavigatorPacketSerializer2.cc
@@ -0,0 +1,54 @@
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program.  If not, see http://www.gnu.org/licenses/.
+//
+
+#include "apps/sNavigator2/SNavigatorPacketSerializer2.h"
+#include "inet/common/packet/serializer/ChunkSerializerRegistry.h"
+#include "SNavigatorPacket2_m.h"
+
+using namespace inet;
+
+Register_Serializer(SNavigatorPacket2, SNavigatorPacketSerializer2);
+
+void SNavigatorPacketSerializer2::serialize(MemoryOutputStream& stream, const Ptr<const Chunk>& chunk) const
+{
+    auto startPosition = stream.getLength();
+    const auto& sNavigatorPacket2 = staticPtrCast<const SNavigatorPacket2>(chunk);
+    stream.writeUint32Be(B(sNavigatorPacket2->getChunkLength()).get());
+    stream.writeUint32Be(sNavigatorPacket2->getIDtalk());
+    stream.writeUint32Be(sNavigatorPacket2->getType());
+    stream.writeUint32Be(std::stoul(sNavigatorPacket2->getNavMessage()));
+
+    int64_t remainders = B(sNavigatorPacket2->getChunkLength() - (stream.getLength() - startPosition)).get();
+    if (remainders < 0)
+        throw cRuntimeError("sNavigatorPacket2 length = %d smaller than required %d bytes", (int)B(sNavigatorPacket2->getChunkLength()).get(), (int)B(stream.getLength() - startPosition).get());
+    stream.writeByteRepeatedly('?', remainders);
+}
+
+const Ptr<Chunk> SNavigatorPacketSerializer2::deserialize(MemoryInputStream& stream) const
+{
+    auto startPosition = stream.getPosition();
+    auto sNavigatorPacket2 = makeShared<SNavigatorPacket2>();
+    B dataLength = B(stream.readUint32Be());
+    sNavigatorPacket2->setIDtalk(stream.readUint32Be());
+    sNavigatorPacket2->setType(stream.readUint32Be());
+    sNavigatorPacket2->setNavMessage(std::to_string(stream.readUint32Be()).c_str());
+
+    B remainders = dataLength - (stream.getPosition() - startPosition);
+    ASSERT(remainders >= B(0));
+    stream.readByteRepeatedly('?', B(remainders).get());
+    return sNavigatorPacket2;
+}
+
+
